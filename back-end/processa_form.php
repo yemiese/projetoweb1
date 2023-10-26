95007fdd8e5e6f35b7eb20bea4f0b9c142511bef
yemiese <uesleim@icloud.com>
2023-10-26 19:17:49 -0300

    Processamento do formulário de inscrição

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -9,7 +9,7 @@ echo '<h1>Dados do usuário</h1>';
 echo '<hr';
 echo "<p>O usuário $nome tem $idade anos </p>";
 
-echo '<a href="http://localhost/udesc/index.html"><button>Home Page</button></a>';
+echo '<a href="http://localhost/udesc/projeto1/front-end/formulario.html"><button>Home Page</button></a>';
 echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
 echo '<button onclick="window.location.href=\'https:\\www.udesc.br\'">Vai para Udesc </button>';
 
